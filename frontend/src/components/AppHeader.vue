a47339df0c428728abf807868838d4f5c27b64a1
Jheavi <javierlaso213@hotmail.com>
2021-03-22 01:37:06 +0100

    add header styles

--- frontend/src/components/AppHeader.vue
+++ frontend/src/components/AppHeader.vue
@@ -1,22 +1,49 @@
 <template>
   <header>
-    <h1>VuePC</h1>
+    <div class="flex2" />
+    <router-link to="/">
+      <h1>
+        VuePC
+      </h1>
+    </router-link>
     <ul>
       <li>Gaming accesories</li>
       <li>Monitors</li>
       <li>Hard Drives</li>
       <li>RAM memories</li>
     </ul>
+    <div class="flex10" />
     <button>Carro</button>
+    <div class="flex1" />
     <span>User</span>
+    <div class="flex2" />
   </header>
 </template>
 
+<script>
+export default {
+  setup () {
+
+  }
+}
+</script>
+
 <style lang="scss" scoped>
 header {
-  display: flex;
   width: 100%;
   height: 70%;
   background-color: #000000;
+  color: #ffffff;
+  display: flex;
+  align-items: center;
 }
+
+ul {
+  display: flex;
+}
+
+li {
+  margin: 0 15px;
+}
+
 </style>
