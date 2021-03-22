b0e8f6a61067cfdf940f24d7d6c22b1bb2877521
Jheavi <javierlaso213@hotmail.com>
2021-03-23 00:21:54 +0100

    add cart route

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import ItemDetail from '../views/ItemDetail.vue'
+import Cart from '../views/Cart.vue'
 
 const routes = [
   {
@@ -13,6 +14,11 @@ const routes = [
     name: 'ItemDetail',
     component: ItemDetail,
     props: true
+  },
+  {
+    path: '/cart',
+    name: 'Cart',
+    component: Cart
   }
 ]
 
