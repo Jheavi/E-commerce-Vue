c986d141499a98c97c607d786f514e9d36f55e88
Jheavi <javierlaso213@hotmail.com>
2021-04-06 14:32:48 +0200

    add details to slideCartDetail component

--- frontend/src/components/SlideCartDetail.vue
+++ frontend/src/components/SlideCartDetail.vue
@@ -1,31 +1,41 @@
 <template>
   <div class="container">
-    <h5>{{ title }}</h5>
+    <h5>{{ item['product-name'] }}</h5>
+    <img
+      :srcset="item['product-image']"
+      :alt="item['product-name']"
+    >
+    <span class="price">{{ item.price }} €</span>
+    <router-link :to="`/detail/${item.id}`">
+      <button>See details</button>
+    </router-link>
   </div>
 </template>
 
 <script>
-import { computed } from 'vue'
-
 export default {
   props: {
     item: { type: Object, default: () => ({}) }
   },
   setup (props) {
-    const title = computed(() => props.item['product-value'])
 
-    return {
-      title
-    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
 .container {
-  width: 200px;
+  width: 185px;
   height: 340px;
   background-color: red($color: #000000);
   display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin: 0 15px;
+}
+
+.price {
+  font-weight: bold;
+  margin-bottom: 30px;
 }
 </style>
