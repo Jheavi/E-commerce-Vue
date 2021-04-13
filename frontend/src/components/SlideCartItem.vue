5a3faa3e096e7a732e9138ef2416cdc3f5413b51
Jheavi <javierlaso213@hotmail.com>
2021-04-13 15:04:44 +0200

    finished slide component, logic and elements to show!

--- frontend/src/components/SlideCartItem.vue
+++ frontend/src/components/SlideCartItem.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="slide-container">
-    <button @click="itemsToRight">
+    <button
+      class="left-arrow"
+      @click="itemsToRight"
+    >
       <i class="el-icon-arrow-left" />
     </button>
     <ul class="slide">
@@ -10,16 +13,20 @@
         :item="item"
       />
     </ul>
-    <button @click="itemsToLeft">
+    <button
+      class="right-arrow"
+      @click="itemsToLeft"
+    >
       <i class="el-icon-arrow-right" />
     </button>
   </div>
 </template>
 
 <script>
-import { computed, onMounted } from 'vue'
+import { computed, onMounted, ref } from 'vue'
 import { useStore } from 'vuex'
 import SlideCartDetail from '@/components/SlideCartDetail.vue'
+import { moveElementToPosition } from '../utils/functions'
 
 export default {
   components: {
@@ -28,20 +35,46 @@ export default {
   setup () {
     const store = useStore()
     const itemList = computed(() => store.state.itemList)
-    const listSliced = computed(() => itemList.value.slice(0, 10))
+    const cartList = computed(() => store.state.cartList)
+    const maxItems = 10
+    const position = ref(0)
+
+    const listSliced = computed(() => itemList.value.filter((item) => (
+      !cartList.value.some((cartItem) => (
+        cartItem.product.id === item.id && cartItem.quantity > 0
+      ))
+    )).slice(0, maxItems))
 
     const itemsToLeft = () => {
+      const slide = document.querySelector('.slide')
+      if (position.value > -listSliced.value.length + 3) position.value -= 1
+      moveElementToPosition(slide, position.value)
     }
 
     const itemsToRight = () => {
+      const slide = document.querySelector('.slide')
+      if (position.value < 0) position.value += 1
+      moveElementToPosition(slide, position.value)
     }
 
     const getItems = () => store.dispatch('getItems')
 
     onMounted(() => {
+      const slide = document.querySelector('.slide')
+      const millisecondsToAutoMoveSlide = 3000
+
       if (!store.state.itemList.length) {
         getItems()
       }
+
+      setInterval(() => {
+        if (position.value > -listSliced.value.length + 3) {
+          position.value -= 1
+        } else if (position.value === -listSliced.value.length + 3) {
+          position.value = 0
+        }
+        moveElementToPosition(slide, position.value)
+      }, millisecondsToAutoMoveSlide)
     })
 
     return {
@@ -58,22 +91,34 @@ i {
   font-size: 30px;
 }
 
+.left-arrow, .right-arrow {
+  position: absolute;
+}
+
+.right-arrow {
+  right: 0;
+}
+
 button {
   width: 30px;
-  background-color: transparent;
+  background-color: #ffffff;
   color: inherit;
   padding: 0;
   height: 300px;
+  z-index: 10;
 }
 
 .slide-container {
   display: flex;
   align-items: center;
+  overflow-x: hidden;
+  position: relative;
 }
 
 .slide {
   display: flex;
-  overflow-x: hidden;
+  transition: transform 0.8s;
+  margin-left: 15px;
 }
 
 </style>
